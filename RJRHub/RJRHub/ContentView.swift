35632d0fc9b2765fadc8355fa147d09fca358572
Rafsan Jani Rupok <109874857+rafsanjanirupok@users.noreply.github.com>
2026-09-06 07:49:57 +0600

    Update ContentView.swift

--- RJRHub/RJRHub/ContentView.swift
+++ RJRHub/RJRHub/ContentView.swift
@@ -1,14 +1,8 @@
-//
-//  ContentView.swift
-//  RJR Hub
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // Your webview code will remain, just without the safe area modifier attached below it
-        WebView(url: URL(string: "https://rafsanjanirupok.com/")!) 
+        WebView()
     }
 }
 
